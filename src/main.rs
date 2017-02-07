f589cdf042686d53c1a52c00dd0007c9e36ef35f
electric coffee <slench102@gmail.com>
2017-02-07 13:10:19 +0100

    Add UUID test

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
 mod data;
 
 fn main() {
-    println!("Hello, Earth!");
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::data::*;
+    #[test]
+    fn ensure_different() {
+        // The participants should be different despite being called with the same parameter
+        // because of how UUIDs work
+        let name = "Johnson".to_string();
+        let p1 = Participant::new(name.clone());
+        let p2 = Participant::new(name);
+        assert_ne!(p1, p2);
+    }
 }
