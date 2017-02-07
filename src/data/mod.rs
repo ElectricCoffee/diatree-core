10477640473db5a1cfe1c797cd077826018410f0
electric coffee <slench102@gmail.com>
2017-02-07 13:09:14 +0100

    make Participant::new public

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -4,15 +4,16 @@ extern crate uuid;
 use self::uuid::Uuid;
 
 /// Conversation Participant, someone who participates in a conversation.
+#[derive(Eq, PartialEq, Ord, PartialOrd, Clone, Debug)]
 pub struct Participant {
     id: Uuid,
     name: String,
 }
 
 impl Participant {
-    fn new(name: String) -> Self {
+    pub fn new(name: String) -> Self {
         let rand_id = Uuid::new_v4(); // ensures the use of a unique identifier for the participant
-        Participant{ id: rand_id, name: name}
+        Participant{ id: rand_id, name: name }
     }
 }
 
