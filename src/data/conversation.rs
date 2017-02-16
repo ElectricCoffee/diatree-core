f9baab15103bd86d0c8202422b6b59405269d8bc
electric coffee <slench102@gmail.com>
2017-02-16 10:16:07 +0100

    add UUID to conversation

--- src/data/conversation.rs
+++ src/data/conversation.rs
@@ -1,7 +1,11 @@
+extern crate uuid;
+use self::uuid::Uuid;
+
 use super::{Participant, Line};
 
 /// A conversation is the actual dialogue that occurs between people
 pub struct Conversation {
+    id: Uuid,
     participants: Vec<Participant>,
     dialogue: Vec<Line>,
 }
