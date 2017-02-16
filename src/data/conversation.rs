61f720051eab499d61d2c6bad8eddbeefda0b8bd
electric coffee <slench102@gmail.com>
2017-02-16 10:16:16 +0100

    add initialisers to conversation

--- src/data/conversation.rs
+++ src/data/conversation.rs
@@ -9,3 +9,18 @@ pub struct Conversation {
     participants: Vec<Participant>,
     dialogue: Vec<Line>,
 }
+
+impl Conversation {
+    pub fn new(parts: Vec<Participant>, lines: Vec<Line>) -> Self {
+        // TODO: add a check to see if the participants are the same as the ones saying the lines
+        Conversation {
+            id: Uuid::new_v4(),
+            participants: parts,
+            dialogue: lines,
+        }
+    }
+
+    pub fn new_single_participant(part: Participant, lines: Vec<Line>) -> Self {
+        Conversation::new(vec![part], lines)
+    }
+}
